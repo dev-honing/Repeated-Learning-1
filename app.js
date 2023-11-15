82a86f200a3eb3495a686980302f6e60bc49b3ba
Ho Nyeong <dev.honing@gmail.com>
2023-11-16 00:22:34 +0900

    익명함수의 선언과 호출

--- app.js
+++ app.js
@@ -3,6 +3,15 @@
 function greet(name) {
   console.log(`Hello, ${name}!`);
 }
-
 // 함수 호출
 greet('Ho Nyeong'); // Hello, Ho Nyeong!
+
+// 2. 익명함수의 선언과 호출
+// 익명함수 선언
+const add = function (a, b) {
+  return a + b;
+};
+// 익명함수 호출
+add(3, 5);
+const result = add(3, 5);
+console.log(result); // 8
