3f5fd04d2e80b1d49d618e9528317af02b9e470c
Ho Nyeong <dev.honing@gmail.com>
2024-04-01 13:12:12 +0900

    Check: CJS 방식의 모듈 제어 점검 - 내보내기

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-// * 1. ESM 방식의 모듈 내보내기(export)
-export function exportFunction() {
+// * 1-1. CJS 방식의 모듈 내보내기(module.exports)
+module.exports = function exportFunction() {
   console.log("내보낸 함수");
-}
+};
+
+// * 1-2. ESM 방식의 모듈 내보내기(export)
+// export function exportFunction() {
+//   console.log("내보낸 함수");
+// }
