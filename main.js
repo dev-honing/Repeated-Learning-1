e60103722c01fc7e2351ed7656c8714a86203ad1
Ho Nyeong <dev.honing@gmail.com>
2024-04-01 13:12:44 +0900

    Check: CJS 방식의 모듈 제어 점검 - 불러오기

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-// * 1. ESM 방식의 모듈 불러오기(import)
-import { exportFunction } from "./app.js";
+// * 1-1. CJS 방식의 모듈 불러오기(require)
+const exportFunction = require("./app.js"); // NPM 패키지를 가져오는 작성법과 비슷한 모습
+
+// * 1-2. ESM 방식의 모듈 불러오기(import)
+// import { exportFunction } from "./app.js";
 
 exportFunction();
