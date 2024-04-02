d2fe31375fb355b7d0e1c4e7bbe475fabcc219df
Ho Nyeong <dev.honing@gmail.com>
2024-04-02 23:26:32 +0900

    Check: 함수의 선언과 호출
    - 무기명 함수

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-// 기명함수 선언
-function namedFunction() {
-  console.log("기명 함수");
-}
+// 무기명 함수 선언
+const anonymous = function() {
+    console.log('무기명 함수');
+};
 
-// 기명함수 호출
-namedFunction();
+// 무기명 함수 호출
+anonymous();
